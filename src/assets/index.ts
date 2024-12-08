f73c7bbeb4b8e54a137176836f8cf7f6ff8dda6a
andreimelo <andreiagno.melo@gmail.com>
2024-12-07 23:50:48 -0600

    refactor: Update card in home and add dummy photo for post card

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,3 @@
 export { default as CollabLogo } from '@assets/CollabLogo.svg';
 export { default as Hammer3D } from '@assets/3diconsHammer.svg';
+export { default as DummyAvatar } from '@assets/Dummy.png';
